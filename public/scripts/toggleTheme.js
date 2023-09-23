c7c5c7a49f1d0dabd5efd30ef2bf5b7c5a3d3ef0
SnowDrive <luc.moralesr@gmail.com>
2023-09-23 02:43:12 -0300

    feat: add toggle theme initial changes

---
+++ public/scripts/toggleTheme.js
@@ -0,0 +1,12 @@
+const toggleTheme = async() => {
+  const { LOCAL_THEME_KEY, DARK_THEME, LIGHT_THEME } = await import('./themeConstants.js')
+  const isCurrentDark = document.body.classList.contains(DARK_THEME)
+  
+  if(isCurrentDark) {
+    document.body.classList.replace(DARK_THEME, LIGHT_THEME)
+    localStorage.setItem(LOCAL_THEME_KEY, LIGHT_THEME)
+  } else {
+    document.body.classList.replace(LIGHT_THEME, DARK_THEME)
+    localStorage.setItem(LOCAL_THEME_KEY, DARK_THEME)
+  }
+}
