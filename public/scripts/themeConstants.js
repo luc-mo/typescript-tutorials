9018e209da7a24edf225b48cda76d1229e2e4e22
SnowDrive <luc.moralesr@gmail.com>
2023-09-24 18:40:04 -0300

    feat: add theme button icons, remove theme assets, remove js theme icon toggle

--- public/scripts/themeConstants.js
+++ public/scripts/themeConstants.js
@@ -2,7 +2,3 @@ export const LOCAL_THEME_KEY = 'theme'
 export const MEDIA_DARK_THEME = '(prefers-color-scheme: dark)'
 export const DARK_THEME = 'dark'
 export const LIGHT_THEME = 'light'
-export const THEME_ICONS = {
-  light: '/assets/sun.svg',
-  dark: '/assets/moon.svg'
-}
